7b8019aeb8da730660027119c945eb228d00c544
artem404 <divine_noodle@mail.ru>
2021-01-30 15:04:36 +0300

    split all into functions

--- main.py
+++ main.py
@@ -46,33 +46,20 @@ def make_screen(wd, name):
     dbx.files_upload(base64.decodebytes(image_code.encode()), "/TEXT/" + name + datetime.datetime.today().strftime("_%d.%m.%Y_%H:%M:%S") + ".png", mute = True)
     return None
   except Exception as e:
-    print("Some error's been occurred with screenshot", e)
+    print("Something goes wrong making a screenshot", e)
     return None
 
-def count_story(wd, stories_dict):
+def authorise(wd):
   try:
-    story_account = WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "FPmhX")))
-    stories_dict[story_account.text] += 1
-    return stories_dict
-  except Exception as e:
-    print("Some error's been occurred with counting story", e)
-    return None
-
-def job(CHROMEDRIVER_PATH, options, username, password):
-  try:
-    wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,chrome_options=options)
-    
     wd.get('https://instagram.com')
     time.sleep(rd.uniform(8, 10))
 
-    time.sleep(rd.uniform(2.5,3.5))
     # wd.find_element_by_name('username').send_keys(username)
+    # wd.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
     # WebDriverWait(wd, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']"))).send_keys(username)
     WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='username']"))).send_keys(username)
     print("Username entered")
-    # wd.find_element_by_xpath("//input[@name=\"username\"]").send_keys(username)
     time.sleep(rd.uniform(0.95,1.45))
-    # wd.find_element_by_name('password').send_keys(password + Keys.ENTER)
     WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='password']"))).send_keys(password)
     print("Password entered")
     time.sleep(rd.uniform(0.95,1.45))
@@ -82,74 +69,96 @@ def job(CHROMEDRIVER_PATH, options, username, password):
     time.sleep(rd.uniform(4.5,5.5))
 
     WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.XPATH, '//button[@type="submit"]'))).click()
-    time.sleep(rd.uniform(6,8))
     print("Submit button's been clicked")
+    time.sleep(rd.uniform(6,8))
 
     make_screen(wd=wd, name="page after login")
     print("Screenshot after login has been made")
+    return wd
+
+  except Exception as e:
+    print("Something goes wrong with username and password autorisation", e)
+    return None
+
+def verify_sms(wd):
+  try:
+    sms_verification = False
+    WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Send Security Code')]"))).click()
     time.sleep(rd.uniform(4.5,5.5))
 
+    make_screen(wd=wd, name="SMS step 1")
+    print("Screenshot SMS Verification 1 has been made")
 
-    try:
-        WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Send Security Code')]"))).click()
-        time.sleep(rd.uniform(4.5,5.5))
+    sms_verification = True
+    waiting_minutes = round(rd.uniform(180, 240) // 60)
 
-        make_screen(wd=wd, name="SMS step 1")
-        print("Screenshot SMS Verification 1 has been made")
+    # send message to my phone number about necessary sms verification
+    twilio_msg = "Your Instagram Bot requires verification, please enter the code during " + str(waiting_minutes) + " minutes!"
+    client = Client(os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN"))
+    client.messages.create(to=os.environ.get("TWILIO_MY_PHONE"),from_=os.environ.get("TWILIO_FAKE_PHONE"),body=twilio_msg)
+    
+    time.sleep(waiting_minutes * 60)
+    # get code from name of file stored in Code dropbox folder
+    for entry in dbx.files_list_folder('/Code').entries:
+      code = entry.name.strip('.txt')
+    print("The SMS code is:", code)
+    WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='security_code']"))).send_keys(code)
+    time.sleep(rd.uniform(4.5,5.5))
 
-        # send message to my phone number about necessary sms verification
-        twilio_msg = "Your Instagram Bot requires verification, please enter the code during 10 minutes!"
-        client = Client(os.environ.get("TWILIO_ACCOUNT_SID"), os.environ.get("TWILIO_AUTH_TOKEN"))
-        client.messages.create(to=os.environ.get("TWILIO_MY_PHONE"),from_=os.environ.get("TWILIO_FAKE_PHONE"),body=twilio_msg)
-        
-        # time.sleep(rd.uniform(600, 650))
-        time.sleep(rd.uniform(180, 240))
-        # get code from name of file stored in Code dropbox folder
-        for entry in dbx.files_list_folder('/Code').entries:
-          code = entry.name.strip('.txt')
-        print("The SMS code is:", code)
-        WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.XPATH, "//input[@name='security_code']"))).send_keys(code)
-        time.sleep(rd.uniform(4.5,5.5))
+    make_screen(wd=wd, name="SMS step 2")
+    print("Screenshot SMS Verification 2 has been made")
 
-        make_screen(wd=wd, name="SMS step 2")
-        print("Screenshot SMS Verification 2 has been made")
+    WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Submit')]"))).click()
+    time.sleep(rd.uniform(4.5,5.5))
 
-        WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'Submit')]"))).click()
-        time.sleep(rd.uniform(4.5,5.5))
+    make_screen(wd=wd, name="SMS step 3")
+    print("Screenshot SMS Verification 3 has been processed")
+    return wd
 
-        make_screen(wd=wd, name="SMS step 3")
-        print("Screenshot SMS Verification 3 has been processed")
+  except Exception as e:
+    if sms_verification is False:
+      print("SMS verification is not needed")
+    else:
+      print("Something goes wrong with SMS verification", e)
+    return None
 
-    except Exception as e:
-      print("Perhaps no sms required", e)
-      pass
+def count_story(wd, stories_dict):
+  try:
+    story_account = WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "FPmhX")))
+    stories_dict[story_account.text] += 1
+    return stories_dict
+  except Exception as e:
+    print("Something goes wrong with counting stories", e)
+    return None
 
-    # It does not require sms every time, after I manually wrote sms code, in the next iteration it's not required to write sms code again
-    # xpath for first button in sms verification /html/body/div[1]/section/div/div/div[3]/form/span/button
-    # xpath for entering digits from sms "//input[@name='security_code]"
-    # xpath for second button in sms verification /html/body/div[1]/section/div/div/div[2]/form/span/button
-
+def watch_story(wd):
+  try:
+    # open the website
     wd.get('https://www.instagram.com/')
     time.sleep(rd.uniform(8, 10))
     print("The webpage 'https://www.instagram.com/' has been opened")
 
+    # make the screenshot of opened main page; it should be authorised
     make_screen(wd=wd, name="Main page")
     print("Screenshot of Main Page has been made")
     time.sleep(rd.uniform(1.5, 2.5))
 
-    # the maximum amount of stories will be 150
+    # create the counter function from collections library
     stories_dict = Counter()
 
+    # click on the story html element
     WebDriverWait(wd, 10).until(EC.element_to_be_clickable((By.CLASS_NAME, "Ckrof"))).click()
     time.sleep(rd.uniform(2.5,3.5))
     # count the first story
     stories_dict = count_story(wd=wd, stories_dict=stories_dict)
 
+    # make the screenshot of the first opened story
     make_screen(wd=wd, name="Story Page")
     print("Screenshot of the first story has been made")
     time.sleep(rd.uniform(0.8,1.2))
 
     start = time.time()
+    # the watching number of stories will be limited to 150
     for count in range(150):
       try:
           time.sleep(rd.uniform(1,1.2))
@@ -166,6 +175,19 @@ def job(CHROMEDRIVER_PATH, options, username, password):
     print("Total number of stories:", count)
     print("The number of stories per account:", stories_dict)
     time.sleep(rd.uniform(2.5,3.5))
+    return None
+  except Exception as e:
+    print("Something goes wrong with watching stories", e)
+    return None
+
+def job(CHROMEDRIVER_PATH, options, username, password):
+  try:
+    wd = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH,chrome_options=options)
+
+    wd = authorise(wd)
+    wd = verify_sms(wd)
+    watch_story(wd)
+
     wd.quit()
   except Exception as e:
     print(e)
